7bcb4126a5c5eecad1501e881ed1f0a9e1e8e60a
Will Leeks <will.leeks@coastdigital.co.uk>
2019-07-04 07:51:24 +0100

    Initial commit

---
+++ pokemon.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+	"math/rand"
+	"strconv"
+	"encoding/json"
+)
+
+type pokemon struct {
+    Name string
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	r := rand.Intn(807)
+
+	resp, err := http.Get("https://pokeapi.co/api/v2/pokemon/" + strconv.Itoa(r))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	
+	defer resp.Body.Close()
+	
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	p := pokemon{}
+	json.Unmarshal([]byte(body), &p)
+
+	n := "Today’s Pokémon is: " + strings.Title(p.Name)
+	fmt.Println(n)
+}
